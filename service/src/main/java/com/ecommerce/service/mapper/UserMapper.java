0c6690d545fe2b5ab246cce1e3d6115ec4186695
PHOENIX\USER <phungphan@wwct.vn>
2025-09-27 11:46:13 +0700

    uuu

--- service/src/main/java/com/ecommerce/service/mapper/UserMapper.java
+++ service/src/main/java/com/ecommerce/service/mapper/UserMapper.java
@@ -5,6 +5,7 @@ import com.ecommerce.service.dto.request.CreateUserRequest;
 import com.ecommerce.service.dto.request.UpdateUserRequest;
 import com.ecommerce.service.dto.response.UserResponse;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
@@ -13,6 +14,7 @@ public interface UserMapper {
     UsersDO toDO(CreateUserRequest request);
 
     // Update -> DO (update vào object sẵn có)
+    @Mapping(target = "id", ignore = true) // ví dụ bỏ qua id khi update
     void updateDO(UpdateUserRequest request, @MappingTarget UsersDO usersDO);
 
     // DO -> Response
